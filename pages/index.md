68235970959c02ad237ffbc1fefee4b8854a8b5c
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2021-03-02 13:23:45 +0100

    Scaffolding

---
+++ pages/index.md
@@ -0,0 +1,3 @@
+# Fragments
+
+[@shoya140 (Shoya Ishimaru)](https://shoya.io) が学んだことや思ったことを記録するブログです。
---
+++ theme.config.js
@@ -0,0 +1,24 @@
+export default {
+  repository: null, // project repo
+  docsRepository: null, // docs repo
+  branch: 'master', // branch of docs
+  path: '/', // path of docs
+  titleSuffix: null,
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: false,
+  footerText: '2021 © Shoya Ishimaru',
+  footerEditOnGitHubLink: true, // will link to the docs repo
+  logo: <>
+    {/* <svg>...</svg> */}
+    <strong>Fragments</strong>
+  </>,
+  head: <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Fragments: weekly updates by shoya140" />
+    <meta name="og:title" content="Fragments: weekly updates by shoya140" />
+  </>
+}
